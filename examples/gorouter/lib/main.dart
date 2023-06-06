0fda8ca3fb97614a742b4afc3082e264a66e428d
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-06 15:06:15 +0100

    Updated GoRouter example to use stateful nested navigation

--- examples/gorouter/lib/main.dart
+++ examples/gorouter/lib/main.dart
@@ -14,62 +14,58 @@ class MyApp extends StatelessWidget {
 
   // private navigators
   final _rootNavigatorKey = GlobalKey<NavigatorState>();
-  final _shellNavigatorKey = GlobalKey<NavigatorState>();
+  final _aNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'jobs');
+  final _bNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'entries');
 
   @override
   Widget build(BuildContext context) {
-    final tabs = [
-      const ScaffoldWithNavBarTabItem(
-        initialLocation: '/a',
-        icon: Icon(Icons.home),
-        label: 'Section A',
-      ),
-      const ScaffoldWithNavBarTabItem(
-        initialLocation: '/b',
-        icon: Icon(Icons.settings),
-        label: 'Section B',
-      ),
-    ];
-
     final goRouter = GoRouter(
       initialLocation: '/a',
       navigatorKey: _rootNavigatorKey,
       debugLogDiagnostics: true,
       routes: [
-        ShellRoute(
-          navigatorKey: _shellNavigatorKey,
-          builder: (context, state, child) {
-            return ScaffoldWithBottomNavBar(tabs: tabs, child: child);
+        // Stateful navigation based on:
+        // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+        StatefulShellRoute.indexedStack(
+          builder: (context, state, navigationShell) {
+            return ScaffoldWithBottomNavBar(navigationShell: navigationShell);
           },
-          routes: [
-            // Products
-            GoRoute(
-              path: '/a',
-              pageBuilder: (context, state) => NoTransitionPage(
-                key: state.pageKey,
-                child: const RootScreen(label: 'A', detailsPath: '/a/details'),
-              ),
-              routes: [
-                GoRoute(
-                  path: 'details',
-                  builder: (context, state) => const DetailsScreen(label: 'A'),
+          branches: [
+            StatefulShellBranch(navigatorKey: _aNavigatorKey, routes: [
+              GoRoute(
+                path: '/a',
+                pageBuilder: (context, state) => NoTransitionPage(
+                  key: state.pageKey,
+                  child:
+                      const RootScreen(label: 'A', detailsPath: '/a/details'),
                 ),
-              ],
-            ),
-            // Shopping Cart
-            GoRoute(
-              path: '/b',
-              pageBuilder: (context, state) => NoTransitionPage(
-                key: state.pageKey,
-                child: const RootScreen(label: 'B', detailsPath: '/b/details'),
+                routes: [
+                  GoRoute(
+                    path: 'details',
+                    builder: (context, state) =>
+                        const DetailsScreen(label: 'A'),
+                  ),
+                ],
               ),
-              routes: [
-                GoRoute(
-                  path: 'details',
-                  builder: (context, state) => const DetailsScreen(label: 'B'),
+            ]),
+            StatefulShellBranch(navigatorKey: _bNavigatorKey, routes: [
+              // Shopping Cart
+              GoRoute(
+                path: '/b',
+                pageBuilder: (context, state) => NoTransitionPage(
+                  key: state.pageKey,
+                  child:
+                      const RootScreen(label: 'B', detailsPath: '/b/details'),
                 ),
-              ],
-            ),
+                routes: [
+                  GoRoute(
+                    path: 'details',
+                    builder: (context, state) =>
+                        const DetailsScreen(label: 'B'),
+                  ),
+                ],
+              ),
+            ]),
           ],
         ),
       ],
@@ -83,54 +79,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// Representation of a tab item in a [ScaffoldWithNavBar]
-class ScaffoldWithNavBarTabItem extends BottomNavigationBarItem {
-  /// Constructs an [ScaffoldWithNavBarTabItem].
-  const ScaffoldWithNavBarTabItem(
-      {required this.initialLocation, required Widget icon, String? label})
-      : super(icon: icon, label: label);
+// Stateful navigation based on:
+// https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+class ScaffoldWithBottomNavBar extends StatelessWidget {
+  const ScaffoldWithBottomNavBar({
+    Key? key,
+    required this.navigationShell,
+  }) : super(key: key ?? const ValueKey<String>('ScaffoldWithBottomNavBar'));
+  final StatefulNavigationShell navigationShell;
 
-  /// The initial location/path
-  final String initialLocation;
-}
-
-class ScaffoldWithBottomNavBar extends StatefulWidget {
-  const ScaffoldWithBottomNavBar(
-      {Key? key, required this.child, required this.tabs})
-      : super(key: key);
-  final Widget child;
-  final List<ScaffoldWithNavBarTabItem> tabs;
-
-  @override
-  State<ScaffoldWithBottomNavBar> createState() =>
-      _ScaffoldWithBottomNavBarState();
-}
-
-class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
-  int _locationToTabIndex(String location) {
-    final index =
-        widget.tabs.indexWhere((t) => location.startsWith(t.initialLocation));
-    // if index not found (-1), return 0
-    return index < 0 ? 0 : index;
-  }
-
-  int get _currentIndex => _locationToTabIndex(GoRouter.of(context).location);
-
-  void _onItemTapped(BuildContext context, int tabIndex) {
-    // Only navigate if the tab index has changed
-    if (tabIndex != _currentIndex) {
-      context.go(widget.tabs[tabIndex].initialLocation);
-    }
+  void _tap(BuildContext context, int index) {
+    navigationShell.goBranch(
+      index,
+      // A common pattern when using bottom navigation bars is to support
+      // navigating to the initial location when tapping the item that is
+      // already active. This example demonstrates how to support this behavior,
+      // using the initialLocation parameter of goBranch.
+      initialLocation: index == navigationShell.currentIndex,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.child,
+      body: navigationShell,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: widget.tabs,
-        onTap: (index) => _onItemTapped(context, index),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: navigationShell.currentIndex,
+        items: const [
+          // products
+          BottomNavigationBarItem(label: 'Section A', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: 'Section B', icon: Icon(Icons.settings)),
+        ],
+        onTap: (index) => _tap(context, index),
       ),
     );
   }
