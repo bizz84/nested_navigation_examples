3471245957c52561ff973eb4d78efbcd069763d8
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-15 14:42:37 +0100

    Make GoRouter example app responsive

--- examples/gorouter/lib/main.dart
+++ examples/gorouter/lib/main.dart
@@ -32,7 +32,8 @@ class MyApp extends StatelessWidget {
         // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
         StatefulShellRoute.indexedStack(
           builder: (context, state, navigationShell) {
-            return ScaffoldWithBottomNavBar(navigationShell: navigationShell);
+            return ScaffoldWithNestedNavigation(
+                navigationShell: navigationShell);
           },
           branches: [
             StatefulShellBranch(
@@ -87,11 +88,12 @@ class MyApp extends StatelessWidget {
 
 // Stateful navigation based on:
 // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
-class ScaffoldWithBottomNavBar extends StatelessWidget {
-  const ScaffoldWithBottomNavBar({
+class ScaffoldWithNestedNavigation extends StatelessWidget {
+  const ScaffoldWithNestedNavigation({
     Key? key,
     required this.navigationShell,
-  }) : super(key: key ?? const ValueKey<String>('ScaffoldWithBottomNavBar'));
+  }) : super(
+            key: key ?? const ValueKey<String>('ScaffoldWithNestedNavigation'));
   final StatefulNavigationShell navigationShell;
 
   void _goBranch(int index) {
@@ -107,15 +109,88 @@ class ScaffoldWithBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 450) {
+        return ScaffoldWithNavigationBar(
+          body: navigationShell,
+          selectedIndex: navigationShell.currentIndex,
+          onDestinationSelected: _goBranch,
+        );
+      } else {
+        return ScaffoldWithNavigationRail(
+          body: navigationShell,
+          selectedIndex: navigationShell.currentIndex,
+          onDestinationSelected: _goBranch,
+        );
+      }
+    });
+  }
+}
+
+class ScaffoldWithNavigationBar extends StatelessWidget {
+  const ScaffoldWithNavigationBar({
+    super.key,
+    required this.body,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
+  final Widget body;
+  final int selectedIndex;
+  final ValueChanged<int> onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
+      body: body,
       bottomNavigationBar: NavigationBar(
-        selectedIndex: navigationShell.currentIndex,
+        selectedIndex: selectedIndex,
         destinations: const [
           NavigationDestination(label: 'Section A', icon: Icon(Icons.home)),
           NavigationDestination(label: 'Section B', icon: Icon(Icons.settings)),
         ],
-        onDestinationSelected: _goBranch,
+        onDestinationSelected: onDestinationSelected,
+      ),
+    );
+  }
+}
+
+class ScaffoldWithNavigationRail extends StatelessWidget {
+  const ScaffoldWithNavigationRail({
+    super.key,
+    required this.body,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
+  final Widget body;
+  final int selectedIndex;
+  final ValueChanged<int> onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+            labelType: NavigationRailLabelType.all,
+            destinations: const <NavigationRailDestination>[
+              NavigationRailDestination(
+                label: Text('Section A'),
+                icon: Icon(Icons.home),
+              ),
+              NavigationRailDestination(
+                label: Text('Section B'),
+                icon: Icon(Icons.settings),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          Expanded(
+            child: body,
+          ),
+        ],
       ),
     );
   }
