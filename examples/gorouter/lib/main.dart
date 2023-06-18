0022c4908bb3168d096744cec29913793d9c3309
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-18 17:30:10 +0100

    Declare GoRouter as a global instance variable (not inline) (#9)

--- examples/gorouter/lib/main.dart
+++ examples/gorouter/lib/main.dart
@@ -3,81 +3,78 @@ import 'package:go_router/go_router.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
-void main() {
-  // turn off the # in the URLs on the web
-  usePathUrlStrategy();
-  runApp(MyApp());
-}
+// private navigators
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorAKey = GlobalKey<NavigatorState>(debugLabel: 'shellA');
+final _shellNavigatorBKey = GlobalKey<NavigatorState>(debugLabel: 'shellB');
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  // private navigators
-  final _rootNavigatorKey = GlobalKey<NavigatorState>();
-  final _shellNavigatorAKey = GlobalKey<NavigatorState>(debugLabel: 'shellA');
-  final _shellNavigatorBKey = GlobalKey<NavigatorState>(debugLabel: 'shellB');
-
-  @override
-  Widget build(BuildContext context) {
-    final goRouter = GoRouter(
-      initialLocation: '/a',
-      // * Passing a navigatorKey causes an issue on hot reload:
-      // * https://github.com/flutter/flutter/issues/113757#issuecomment-1518421380
-      // * However it's still necessary otherwise the navigator pops back to
-      // * root on hot reload
-      navigatorKey: _rootNavigatorKey,
-      debugLogDiagnostics: true,
-      routes: [
-        // Stateful navigation based on:
-        // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
-        StatefulShellRoute.indexedStack(
-          builder: (context, state, navigationShell) {
-            return ScaffoldWithNestedNavigation(
-                navigationShell: navigationShell);
-          },
-          branches: [
-            StatefulShellBranch(
-              navigatorKey: _shellNavigatorAKey,
+final goRouter = GoRouter(
+  initialLocation: '/a',
+  // * Passing a navigatorKey causes an issue on hot reload:
+  // * https://github.com/flutter/flutter/issues/113757#issuecomment-1518421380
+  // * However it's still necessary otherwise the navigator pops back to
+  // * root on hot reload
+  navigatorKey: _rootNavigatorKey,
+  debugLogDiagnostics: true,
+  routes: [
+    // Stateful navigation based on:
+    // https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return ScaffoldWithNestedNavigation(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorAKey,
+          routes: [
+            GoRoute(
+              path: '/a',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: RootScreen(label: 'A', detailsPath: '/a/details'),
+              ),
               routes: [
                 GoRoute(
-                  path: '/a',
-                  pageBuilder: (context, state) => const NoTransitionPage(
-                    child: RootScreen(label: 'A', detailsPath: '/a/details'),
-                  ),
-                  routes: [
-                    GoRoute(
-                      path: 'details',
-                      builder: (context, state) =>
-                          const DetailsScreen(label: 'A'),
-                    ),
-                  ],
+                  path: 'details',
+                  builder: (context, state) => const DetailsScreen(label: 'A'),
                 ),
               ],
             ),
-            StatefulShellBranch(
-              navigatorKey: _shellNavigatorBKey,
+          ],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorBKey,
+          routes: [
+            // Shopping Cart
+            GoRoute(
+              path: '/b',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: RootScreen(label: 'B', detailsPath: '/b/details'),
+              ),
               routes: [
-                // Shopping Cart
                 GoRoute(
-                  path: '/b',
-                  pageBuilder: (context, state) => const NoTransitionPage(
-                    child: RootScreen(label: 'B', detailsPath: '/b/details'),
-                  ),
-                  routes: [
-                    GoRoute(
-                      path: 'details',
-                      builder: (context, state) =>
-                          const DetailsScreen(label: 'B'),
-                    ),
-                  ],
+                  path: 'details',
+                  builder: (context, state) => const DetailsScreen(label: 'B'),
                 ),
               ],
             ),
           ],
         ),
       ],
-    );
+    ),
+  ],
+);
 
+void main() {
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: goRouter,
       debugShowCheckedModeBanner: false,
